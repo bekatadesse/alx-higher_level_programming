62bcdb857b7bab8be01b97cd8b8c923381c376d4
bekatadesse <bekatadesse5@gmail.com>
2022-10-04 07:47:15 +0300

    Update 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
-import {argv} from 'node:process'
-
-if(argv.length = 2){
-    console.log('Argument found');
+if (process.argv.length < 3) {
+  console.log('No argument');
 }
-else if(argv.length >= 3){
-    console.log('Arguments found');
+else if (process.argv.length === 3) {
+  console.log('Argument found');
 }
-else{
-    console.log('No argument');
+else {
+  console.log('Arguments found');
 }
