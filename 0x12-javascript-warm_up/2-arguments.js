0742e8458cf821f20dc245fe149fa5c6e1a355b3
bekatadesse <bekatadesse5@gmail.com>
2022-10-04 07:37:01 +0300

    Create 2-arguments.js

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+import {argv} from 'node:process'
+
+if(argv.length = 0){
+    console.log('Argument found');
+}
+else if(argv.length >= 1){
+    console.log('Arguments found');
+}
+else{
+    console.log('Argument found');
+}
