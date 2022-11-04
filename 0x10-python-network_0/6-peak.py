66797b485ba568227c39db49dce480d9635c713b
bekatadesse <bekatadesse5@gmail.com>
2022-11-04 15:17:20 +0300

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,10 +1,23 @@
 #!/usr/bin/python3
 """Technical interview preparation:"""
 
+
 def find_peak(list_of_integers):
-    """ function that finds a peak in a list"""
-    arr = sorted(list_of_integers)
+    """function that finds a peak in a list"""
     if list_of_integers == []:
         return None
-    return(arr[-1])    
 
+    size = len(list_of_integers)
+    if size == 1:
+        return list_of_integers[0]
+    elif size == 2:
+        return max(list_of_integers)
+
+    mid = int(size / 2)
+    peak = list_of_integers[mid]
+    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+        return peak
+    elif peak < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
+    else:
+        return find_peak(list_of_integers[mid + 1:])
