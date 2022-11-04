ef9b83350fcfaba8a8158535db7f95e51c490999
bekatadesse <bekatadesse5@gmail.com>
2022-11-04 14:48:35 +0300

    checking what the checker will score

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Technical interview preparation:"""
+
+def find_peak(list_of_integers):
+  """ function that finds a peak in a list"""
+    arr=sorted(list_of_integers)
+    if list_of_integers == []:
+        return None
+    return(arr[-1])    
+
