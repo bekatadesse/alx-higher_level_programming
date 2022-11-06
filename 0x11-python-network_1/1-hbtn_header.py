2c5e46b3b25348bd4844981dcd1655a51b30ecc2
bekatadesse <bekatadesse5@gmail.com>
2022-11-06 15:55:35 +0300

    update

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+script that use the package urllib and fetches url
+and show body of the response
+"""
+import urllib.request
+import sys
+url = sys.argv[1]
+request = urllib.request.Request(url)
+with urllib.request.urlopen(request) as response:
+    print(dict(response.headers).get("X-Request-Id"))
