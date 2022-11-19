8b798e204a70eae95486133bb8f107c09b111851
bekatadesse <bekatadesse5@gmail.com>
2022-11-19 15:53:48 +0300

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,6 +5,7 @@ import requests
 
 if __name__ == "__main__":
     r = requests.get("http://0.0.0.0:5050/status")
+    R = r.text
     print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+    print("\t- type: {}".format(type(R)))
+    print("\t- content: {}".format(R))
