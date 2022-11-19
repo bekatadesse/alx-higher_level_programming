84c489af85b75354fe319220954c9ebcc78fa031
bekatadesse <bekatadesse5@gmail.com>
2022-11-19 09:42:54 +0000

    update

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -10,5 +10,4 @@ try:
     with request.urlopen(sys.argv[1]) as respons:
         print(respons.read().decode('UTF-8'))
 except error.HTTPError as e:
-    # print(e.code)
-    # print(e.read())
+    print('Error code:', e.code)
