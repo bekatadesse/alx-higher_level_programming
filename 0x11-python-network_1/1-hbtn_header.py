6ab5f67bd60b14da3e673451b9107b7082f1b97e
bekatadesse <bekatadesse5@gmail.com>
2022-11-19 09:57:35 +0000

    add

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """A script that:
+- You must use the packages urllib and sys
+- You are not allow to import packages other than urllib and sys
 -script that use the package urllib and sys.
 -fetches url and show body of the response.
 """
@@ -8,6 +10,8 @@ import sys
 import urllib.request
 
 if __name__ == "__main__":
-    with urllib.request.urlopen(sys.argv[1]) as response:
+    url = sys.argv[1]
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
         html = response.info()
         print(html.get('X-Request-Id'))
