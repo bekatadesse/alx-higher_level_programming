9e065f3234b882f63821a63d13548f62ba9ea382
bekatadesse <bekatadesse5@gmail.com>
2022-11-19 20:27:23 +0300

    Update 7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""
-- Displays the X-Request-Id header variable of a request to a given URL
-- You must use the packages requests and sys
+"""A script that
+- takes in a URL
+- sends a request to the URL
+- displays the body of the response.
 """
 import sys
 import requests
@@ -9,9 +10,9 @@ import requests
 
 if __name__ == "__main__":
     url = sys.argv[1]
+
     r = requests.get(url)
-    x = r.status_code
-    if x >= 400:
-        print(f"Error code: {x}")
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
     else:
         print(r.text)
