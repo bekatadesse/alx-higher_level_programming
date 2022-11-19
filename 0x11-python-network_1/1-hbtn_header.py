3b98035e1ae20e3ce9ffdc19cef8e66a0595770a
bekatadesse <bekatadesse5@gmail.com>
2022-11-19 09:54:24 +0000

    add

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,7 +7,7 @@
 import sys
 import urllib.request
 
-
-with urllib.request.urlopen(sys.argv[1]) as response:
-    html = response.info()
-    print(html.get('X-Request-Id'))
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        html = response.info()
+        print(html.get('X-Request-Id'))
